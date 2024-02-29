f3a30eabdfb3ee93c5dfdea9be82aa9ad9c8515f
237031900 <155444966+237031900@users.noreply.github.com>
2024-02-29 12:44:35 +0000

    test v

---
+++ controllers/auth.ts
@@ -0,0 +1,26 @@
+import passport from "koa-passport";
+import { BasicStrategy} from "passport-http"
+import {RouterContext } from "koa-router"
+
+passport.use(new BasicStrategy(async (username, password, done) => {
+  if(username === "admin" && password === "password"){
+    done(null, {username: "admin"})
+  }else{
+    done(null, false)
+  }
+}))
+
+export const basicAuth = async(ctx: RouterContext, next: any) => {
+  await passport.authenticate("basic", {session: false})(ctx, next)
+
+  if(ctx.status === 401){
+    ctx.body = {
+      message: 'you are not authorized'
+    }
+  }
+  else{
+    ctx.body = {
+      message: 'you are passed'
+    }
+  }
+}
